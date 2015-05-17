e5969e0afa6fc6ef218ac031f5a6850b3cccdd27
Graham Alexander-Thomson <industrialzombie@gmail.com>
2015-05-17 12:03:03 -0600

    Added missing file
    I still don't understand git well enough to avoid committing things with
    missing files. Sorry!

---
+++ XNA/NetRunner.Core/Actions/MakeCardVisible.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetRunner.Core.Actions
+{
+    public class MakeCardVisible : ActionBase
+    {
+        public object CardIdentifier { get; private set; }
+        public string CardName { get; private set; }
+
+        public MakeCardVisible(object cardIdentifier, string cardName)
+        {
+            CardIdentifier = cardIdentifier;
+            CardName = cardName;
+        }
+
+        public override bool IsValid(GameContext context, GameFlow.StateMachine stateMachine)
+        {
+            //TODO: Ensure cardIdentifier references a card that exists.
+            //return CardIdentifier.CardExists(context);
+            return true;
+        }
+
+        public override void Apply(GameContext context, GameFlow.StateMachine stateMachine)
+        {
+            //TODO: Attach the revealed behaviour to the card.
+            //Card card = CardIdentifier.FindCard(context);
+            //card.Behaviour = CardBehaviourFactory.Create(CardName);
+        }
+
+        protected override bool Equals(ActionBase otherAction)
+        {
+            // TODO: This action will never be compared as it originates from the hosted game
+            // but we should still implement this :)
+            return true;
+        }
+    }
+}
